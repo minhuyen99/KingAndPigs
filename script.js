bf77b7bd1a129220a6786cfa98ed0eb203acbb66
minhuyen0210 <uyen.mdc@consultindochina.com>
2023-01-18 15:54:08 +0700

    create player

--- script.js
+++ script.js
@@ -4,5 +4,39 @@ const c = canvas.getContext('2d')
 canvas.width = 64*16 // 1024
 canvas.height = 64*9 // 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+class Player {
+	constructor(){
+		this.position = { x: 100, y: 100 }
+		this.width = 100
+		this.height = 100
+		this.sides = {
+			bottom: this.position.y + this.height
+		}
+	}
+
+	draw(){
+		c.fillStyle = 'red'
+		c.fillRect(this.position.x, this.position.y, this.width, this.height)
+	}
+
+	update(){
+		if (this.sides.bottom < canvas.height){
+			this.position.y++
+			this.sides.bottom = this.position.y + this.height
+		}
+
+	}
+}
+
+const player = new Player()
+
+function animate(){
+	requestAnimationFrame(animate)
+	c.fillStyle = 'white'
+	c.fillRect(0, 0, canvas.width, canvas.height)
+
+	player.draw()
+	player.update()
+}
+
+animate()
