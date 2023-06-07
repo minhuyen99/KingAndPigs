0ed792ecafb576bd53088ac7105abfa48e0fbb89
brian <brian.hoag93@gmail.com>
2023-06-07 11:31:00 +0700

    collision blocks

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ const keys = {
 	}
 }
 
+// -------------- sprite -------------- //
 class Sprite{
 	constructor({position, imageSrc}){
 		this.position = position;
@@ -40,7 +41,11 @@ const backgroundLevel1 = new Sprite({
 	},
 	imageSrc: './img/backgroundLevel1.png'
 })
+// -------------- sprite -------------- //
 
+
+
+// -------------- player -------------- //
 class Player {
 	constructor(){
 		this.position = { x: 100, y: 100 }
@@ -71,11 +76,73 @@ class Player {
 }
 
 const player = new Player()
+// -------------- player -------------- //
+
+
+// -------------- collision blocks -------------- //
+const collisionsLevel1 = [
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,292,292,292,292,292,292,292,292,292,292,292,292,292,292,0,
+    0,292,0,0,0,0,0,0,0,0,0,0,0,0,292,0,
+    0,292,0,0,0,0,0,0,0,0,0,0,0,0,292,0,
+    0,292,292,0,0,0,0,0,0,0,0,0,0,0,292,0,
+    0,292,292,292,292,292,292,292,292,292,292,292,292,292,292,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+]
+
+Array.prototype.parse2D = function (){
+    const rows = []
+    for (let i = 0; i < this.length; i+=16){
+        rows.push(this.slice(i, i+16))
+    }
+    return rows;
+}
+
+Array.prototype.createObjectsFrom2D = function(){
+    const objects = []
+    this.forEach((row, y) => {
+        row.forEach((symbol, x) => {
+            if (symbol == 292){
+                // push a new collision into collisionblocks array
+                objects.push(new CollisionBlock({
+                    position: {
+                        x: x*64,
+                        y: y*64
+                    }
+                }))
+            }
+        })
+    });
+
+    return objects;
+}
+
+class CollisionBlock{
+    constructor({position}){
+        this.position = position
+        this.width = 64
+        this.height = 64
+    }
+
+    draw(){
+        c.fillStyle = 'rgba(255, 0, 0, 0.5)'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+const parsedCollisions = collisionsLevel1.parse2D()
+const collisionBlocks = parsedCollisions.createObjectsFrom2D()
+// -------------- collision blocks -------------- //
 
 function animate(){
 	requestAnimationFrame(animate)
 
 	backgroundLevel1.draw()
+
+	collisionBlocks.forEach(collision => {
+		collision.draw()
+	})
 
 	player.velocity.x = 0
 	if (keys.d.pressed) player.velocity.x = 1
