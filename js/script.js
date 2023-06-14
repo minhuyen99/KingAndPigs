97052f05aa7b5e0c56a55d11d71cb5dfb47b53ef
brian <brian.hoag93@gmail.com>
2023-06-14 20:06:37 +0700

    collision detection

--- js/script.js
+++ js/script.js
@@ -45,38 +45,7 @@ const backgroundLevel1 = new Sprite({
 
 
 
-// -------------- player -------------- //
-class Player {
-	constructor(){
-		this.position = { x: 100, y: 100 }
-		this.velocity = { x: 0, y: 0 }
-		this.width = 100
-		this.height = 100
-		this.sides = {
-			bottom: this.position.y + this.height
-		}
-		this.gravity = 1
-	}
 
-	draw(){
-		c.fillStyle = 'red'
-		c.fillRect(this.position.x, this.position.y, this.width, this.height)
-	}
-
-	update(){
-		this.position.y += this.velocity.y
-		this.position.x += this.velocity.x
-		this.sides.bottom = this.position.y + this.height
-
-		if (this.sides.bottom + this.velocity.y < canvas.height){
-			this.velocity.y += this.gravity
-		}
-		else this.velocity.y = 0
-	}
-}
-
-const player = new Player()
-// -------------- player -------------- //
 
 
 // -------------- collision blocks -------------- //
@@ -135,6 +104,101 @@ const parsedCollisions = collisionsLevel1.parse2D()
 const collisionBlocks = parsedCollisions.createObjectsFrom2D()
 // -------------- collision blocks -------------- //
 
+// -------------- player -------------- //
+class Player {
+	constructor({collisionBlocks = []}){
+		this.position = { x: 200, y: 200 }
+		this.velocity = { x: 0, y: 0 }
+		this.width = 25
+		this.height = 25
+		this.sides = {
+			bottom: this.position.y + this.height
+		}
+		this.gravity = 1
+
+		this.collisionBlocks = collisionBlocks
+	}
+
+	draw(){
+		c.fillStyle = 'red'
+		c.fillRect(this.position.x, this.position.y, this.width, this.height)
+	}
+
+
+	checkForHorizontalCollisions(){
+		for (let i = 0; i < this.collisionBlocks.length; i++) {
+			const collisionBlock = this.collisionBlocks[i];
+			
+			// if the collision exists
+			if (this.position.x <= collisionBlock.position.x + collisionBlock.width && 
+				this.position.x + this.width >= collisionBlock.position.x &&
+				this.position.y + this.height >= collisionBlock.position.y &&
+				this.position.y <= collisionBlock.position.y + collisionBlock.height
+				){
+					// collision on x axis going to the left
+					if (this.velocity.x < 0){
+						this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01
+						break
+					}
+
+					if (this.velocity.x > 0){
+						this.position.x = collisionBlock.position.x - this.width - 0.01
+						break
+					}
+			}
+		}
+	}
+
+	applyGravity(){
+		this.velocity.y += this.gravity
+		this.position.y += this.velocity.y
+	}
+
+	checkForVerticalCollisions(){
+		for (let i = 0; i < this.collisionBlocks.length; i++) {
+			const collisionBlock = this.collisionBlocks[i];
+			
+			// if the collision exists
+			if (this.position.x <= collisionBlock.position.x + collisionBlock.width && 
+				this.position.x + this.width >= collisionBlock.position.x &&
+				this.position.y + this.height >= collisionBlock.position.y &&
+				this.position.y <= collisionBlock.position.y + collisionBlock.height
+				){
+					// collision on x axis going to the left
+					if (this.velocity.y < 0){
+						this.velocity.y = 0
+						this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01
+						break
+					}
+
+					if (this.velocity.y > 0){
+						this.velocity.y = 0
+						this.position.y = collisionBlock.position.y - this.height - 0.01
+						break
+					}
+			}
+		}
+	}
+
+	update(){
+		this.position.x += this.velocity.x
+		
+		// check for horzontal collisions
+		this.checkForHorizontalCollisions()
+
+		// apply gravity
+		this.applyGravity()
+
+		// check for vertical collisions
+		this.checkForVerticalCollisions()
+	}
+}
+
+const player = new Player({
+	collisionBlocks
+})
+// -------------- player -------------- //
+
 function animate(){
 	requestAnimationFrame(animate)
 
@@ -156,7 +220,7 @@ addEventListener('keydown', (event) => {
 	switch (event.key){
 		case 'w':
 		case 'ArrowUp':
-			if (player.velocity.y == 0) player.velocity.y = -20
+			if (player.velocity.y == 0) player.velocity.y = -25
 			break
 		case 'a':
 		case 'ArrowLeft':
