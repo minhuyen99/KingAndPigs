82b64b608485c76593944878109e43b8fa9885f6
brian <brian.hoag93@gmail.com>
2023-07-07 21:38:50 +0700

    gravity

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -4,11 +4,17 @@ class Player {
             x: 100,
             y: 100
         }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
         this.width = 100
         this.height = 100
         this.sides = {
             bottom: this.position.y + this.height
         }
+        this.gravity = 1
     }
 
     draw(){
@@ -17,9 +23,15 @@ class Player {
     }
 
     update(){
-        if (this.sides.bottom < canvas.height){
-            this.position.y++
-            this.sides.bottom = this.position.y + 100
+        this.position.y += this.velocity.y
+
+        // above bottom of canvas
+        if (this.sides.bottom + this.velocity.y < canvas.height){
+            this.velocity.y += this.gravity
+            this.sides.bottom = this.position.y + this.height
+        }
+        else {
+            this.velocity.y = 0
         }
     }
 }
