600c559ea2adabd982d5db5d4724b8378d70f66e
brian <brian.hoag93@gmail.com>
2023-07-08 09:45:19 +0700

    collision detection

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,20 +1,24 @@
 class Player {
-    constructor(){
+    constructor({
+        collisionBlocks = []
+    }){
         this.position = {
-            x: 100,
-            y: 100
+            x: 200,
+            y: 200
         }
         this.velocity = {
             x: 0,
             y: 0
         }
 
-        this.width = 100
-        this.height = 100
+        this.width = 25
+        this.height = 25
         this.sides = {
             bottom: this.position.y + this.height
         }
         this.gravity = 1
+
+        this.collisionBlocks = collisionBlocks
     }
 
     draw(){
@@ -22,17 +26,67 @@ class Player {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
+    checkForHorizontalCollisions(){
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const block = this.collisionBlocks[i];
+
+            // if a collision exists
+            if (this.position.x <= block.position.x + block.width &&
+                this.position.x + this.width >= block.position.x &&
+                this.position.y + this.height >= block.position.y &&
+                this.position.y <= block.position.y + block.height){
+                    if (this.velocity.x < 0){
+                        this.position.x = block.position.x + block.width + 0.01
+                        break
+                    }
+
+                    if (this.velocity.x > 0){
+                        this.position.x = block.position.x - this.width - 0.01
+                        break
+                    }
+            }
+        }
+    }
+
+    checkForVerticalCollisions(){
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const block = this.collisionBlocks[i];
+
+            // if a collision exists
+            if (this.position.x <= block.position.x + block.width &&
+                this.position.x + this.width >= block.position.x &&
+                this.position.y + this.height >= block.position.y &&
+                this.position.y <= block.position.y + block.height){
+                    if (this.velocity.y < 0){
+                        this.velocity.y = 0
+                        this.position.y = block.position.y + block.height + 0.01
+                        break
+                    }
+
+                    if (this.velocity.y > 0){
+                        this.velocity.y = 0
+                        this.position.y = block.position.y - this.height - 0.01
+                        break
+                    }
+            }
+            
+        }
+    }
+
+    applyGravity(){
+        this.velocity.y += this.gravity
+        this.position.y += this.velocity.y
+    }
+
     update(){
         this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        this.sides.bottom = this.position.y + this.height
+        // check for horizontal collisions
+        this.checkForHorizontalCollisions()
 
-        // above bottom of canvas
-        if (this.sides.bottom + this.velocity.y < canvas.height){
-            this.velocity.y += this.gravity
-        }
-        else {
-            this.velocity.y = 0
-        }
+        // apply gravity
+        this.applyGravity()
+
+        // check for vertical collisions
+        this.checkForVerticalCollisions()
     }
 }
